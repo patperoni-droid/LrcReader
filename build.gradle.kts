69c9518997e37dae2f284809bdead4554ab60377
Patrick <patperoni@gmail.com>
2026-07-21 15:48:12 +0200

    fix(build): update dex toolchain for sdk 35

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // Plugin Android + Kotlin pour tout le projet
-    id("com.android.application") version "8.5.0" apply false
+    id("com.android.application") version "8.12.3" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.kapt") version "2.0.21" apply false
 
