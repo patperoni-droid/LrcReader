872eb47372726d75eef2c05d7bf618c9538bd692
Patrick <patperoni@gmail.com>
2026-07-21 15:33:16 +0200

    fix(build): update compose compiler patch level

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     // Plugin Android + Kotlin pour tout le projet
     id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.kapt") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.kapt") version "2.0.21" apply false
 
     // ✅ Nouveau plugin obligatoire depuis Kotlin 2.0
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
