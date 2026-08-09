27075ca2d08ea34ecc8ccb1dab39e92fa8be03f4
Patrick <patperoni@gmail.com>
2026-08-09 23:17:09 +0200

    test(prompter): align key mapping tests with current controls

--- app/src/test/java/com/patrick/lrcreader/ui/PrompterKeyMappingTest.kt
+++ app/src/test/java/com/patrick/lrcreader/ui/PrompterKeyMappingTest.kt
@@ -8,41 +8,29 @@ import org.junit.Test
 class PrompterKeyMappingTest {
 
     @Test
-    fun mapPrompterKey_mapsNextKeys() {
+    fun mapPrompterKey_mapsRightToNext() {
+        assertEquals(PrompterAction.NEXT, mapPrompterKey(KeyEvent.KEYCODE_DPAD_RIGHT))
+    }
+
+    @Test
+    fun mapPrompterKey_mapsLeftToPrev() {
+        assertEquals(PrompterAction.PREV, mapPrompterKey(KeyEvent.KEYCODE_DPAD_LEFT))
+    }
+
+    @Test
+    fun mapPrompterKey_returnsNullForUnsupportedNavigationAndToggleKeys() {
         val keys = listOf(
             KeyEvent.KEYCODE_PAGE_DOWN,
-            KeyEvent.KEYCODE_DPAD_DOWN,
-            KeyEvent.KEYCODE_DPAD_RIGHT
-        )
-
-        keys.forEach { keyCode ->
-            assertEquals(PrompterAction.NEXT, mapPrompterKey(keyCode))
-        }
-    }
-
-    @Test
-    fun mapPrompterKey_mapsPrevKeys() {
-        val keys = listOf(
             KeyEvent.KEYCODE_PAGE_UP,
+            KeyEvent.KEYCODE_DPAD_DOWN,
             KeyEvent.KEYCODE_DPAD_UP,
-            KeyEvent.KEYCODE_DPAD_LEFT
-        )
-
-        keys.forEach { keyCode ->
-            assertEquals(PrompterAction.PREV, mapPrompterKey(keyCode))
-        }
-    }
-
-    @Test
-    fun mapPrompterKey_mapsToggleKeys() {
-        val keys = listOf(
             KeyEvent.KEYCODE_SPACE,
             KeyEvent.KEYCODE_ENTER,
             KeyEvent.KEYCODE_NUMPAD_ENTER
         )
 
         keys.forEach { keyCode ->
-            assertEquals(PrompterAction.TOGGLE, mapPrompterKey(keyCode))
+            assertNull(mapPrompterKey(keyCode))
         }
     }
 
